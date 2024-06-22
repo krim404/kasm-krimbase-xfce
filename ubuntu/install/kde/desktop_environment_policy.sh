0cd5f85e463f25cb1471c28a626abcd3d0c8a48c
krim <authentik@krim.me>
2024-06-22 13:41:43 +0000

    Update 75 files
    - /common/startup_scripts/emblems.sh
    - /common/startup_scripts/generate_container_user
    - /common/startup_scripts/kasm_default_profile.sh
    - /common/startup_scripts/set_user_permission.sh
    - /common/startup_scripts/trustdesktop.sh
    - /common/startup_scripts/vnc_startup.sh
    - /common/install/kasm_vnc/kasmvnc.yaml
    - /common/install/profile_sync/kasm_background_profile_sync.sh
    - /common/resources/images/bg_almalinux.png
    - /common/resources/images/bg_alpine.png
    - /common/resources/images/bg_bionic.png
    - /common/resources/images/bg_centos.png
    - /common/resources/images/bg_debian.svg
    - /common/resources/images/bg_fedora.png
    - /common/resources/images/bg_focal.png
    - /common/resources/images/bg_jammy.png
    - /common/resources/images/bg_kasm.png
    - /common/resources/images/bg_noble.png
    - /common/resources/images/bg_opensuse.png
    - /common/resources/images/bg_oracle.png
    - /common/resources/images/bg_parrotos6.jpg
    - /common/resources/images/bg_remnux.png
    - /common/resources/images/bg_rocky.png
    - /common/resources/images/icon_kasm.png
    - /common/resources/images/icon_ubuntu.png
    - /common/scripts/kasm_hook_scripts/kasm_post_run_root.sh
    - /common/scripts/kasm_hook_scripts/kasm_post_run_user.sh
    - /common/scripts/kasm_hook_scripts/kasm_pre_shutdown_root.sh
    - /common/scripts/kasm_hook_scripts/kasm_pre_shutdown_user.sh
    - /common/.DS_Store
    - /ubuntu/install/audio/install_audio.sh
    - /ubuntu/install/audio_input/install_audio_input.sh
    - /ubuntu/install/cleanup/cleanup.sh
    - /ubuntu/install/cursors/cursor-aero.tar.gz
    - /ubuntu/install/cursors/cursor-bridge.tar.gz
    - /ubuntu/install/cursors/cursor-capitaine-r4.tar.gz
    - /ubuntu/install/cursors/install_cursors.sh
    - /ubuntu/install/emblems/install_emblems.sh
    - /ubuntu/install/extra/kali.sh
    - /ubuntu/install/extra/noop.sh
    - /ubuntu/install/extra/remnux.sh
    - /ubuntu/install/fonts/install_custom_fonts.sh
    - /ubuntu/install/gamepad/gamepad.svg
    - /ubuntu/install/gamepad/install_gamepad.sh
    - /ubuntu/install/kasm_upload_server/install_kasm_upload_server.sh
    - /ubuntu/install/kasm_vnc/install_kasm_vnc.sh
    - /ubuntu/install/kde/auto_start.desktop
    - /ubuntu/install/kde/desktop_environment_policy.sh
    - /ubuntu/install/kde/install_kde.sh
    - /ubuntu/install/langpacks/install_langpacks.sh
    - /ubuntu/install/maximize_script/maximize_window.sh
    - /ubuntu/install/nvidia/10_nvidia.json
    - /ubuntu/install/openbox/install_openbox.sh
    - /ubuntu/install/package_rules/package_rules.sh
    - /ubuntu/install/profile_sync/install_profile_sync.sh
    - /ubuntu/install/recorder/install_recorder.sh
    - /ubuntu/install/sysbox/install_systemd.sh
    - /ubuntu/install/tools/install_tools.sh
    - /ubuntu/install/unison/install_unison.sh
    - /ubuntu/install/virtualgl/install_virtualgl.sh
    - /ubuntu/install/virtualgl/virtualgl_3.0.80_amd64.deb
    - /ubuntu/install/virtualgl/virtualgl_3.0.80_arm64.deb
    - /ubuntu/install/webcam/install_webcam.sh
    - /ubuntu/install/xfce/install_xfce_ui.sh
    - /ubuntu/install/printer/resources/kasm.ppd
    - /ubuntu/install/squid/install/install_squid.sh
    - /ubuntu/install/printer/install_printer.sh
    - /ubuntu/install/printer/start_cups.sh
    - /ubuntu/install/squid/resources/error_message/access_denied.html
    - /ubuntu/install/squid/resources/SN.png
    - /ubuntu/install/squid/resources/squid.conf
    - /ubuntu/install/squid/resources/ssl_bump_bypass_domains.conf
    - /ubuntu/install/squid/resources/ssl_bump_bypass_ips.conf
    - /ubuntu/install/squid/resources/start_squid.sh
    - /ubuntu/install/.DS_Store

--- ubuntu/install/kde/desktop_environment_policy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# TODO: Find explicit way to determine that KDE is ready
-sleep 5
-
-# Disable screen lock
-kwriteconfig5 --file kscreenlockerrc --group Daemon --key Autolock false
-qdbus org.freedesktop.ScreenSaver /ScreenSaver configure
